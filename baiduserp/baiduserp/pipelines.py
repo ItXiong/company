09d41dc27255b73452fc2202f3be59afe6863734
itxiong <7928408@qq.com>
2018-10-20 17:48:40 +0800

    deleted:    baiduserp/all.log 	modified:   baiduserp/baiduserp/pipelines.py

--- baiduserp/baiduserp/pipelines.py
+++ baiduserp/baiduserp/pipelines.py
@@ -6,7 +6,6 @@ from scrapy.selector import Selector
 from baiduserp.executemysql import ExecuteMysql
 
 # Define your item pipelines here
-#
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
